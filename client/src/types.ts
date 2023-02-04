e0a8a2a28ade809a9be47420d03a5d3f3d680eb7
bsinoimeri <bela.sinoimeri@xentral.com>
2023-02-04 17:37:37 +0100

    Included Global types

---
+++ client/src/types.ts
@@ -0,0 +1,16 @@
+/* Global types */
+
+export type Person = {
+  district: string;
+  link: string;
+  name: string;
+  office: string;
+  party: string;
+  phone: string;
+  state: string;
+};
+
+export type Option = {
+  label: string;
+  value: string;
+};
